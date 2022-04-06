3652c9e98f169ac12dad9474e9abf86a10e461fa
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-06 07:31:13 -0700

    Fix errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,30 +1,25 @@
 const ADD_BOOK = 'book-store/books/ADD_BOOK';
 const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [{title: 'Think Big', author: 'Brian Tracy'}];
 
 export const addbook = (payload) => ({
-  index: 0,
   type: ADD_BOOK,
   payload,
 });
 
 export const removebook = (payload) => ({
-  index: 0,
   type: REMOVE_BOOK,
   payload,
 });
 
-const reducer = (action, state = initialState) => {
-  switch (action) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
 
     case REMOVE_BOOK:
-      return state.map((book, index) => {
-        if (action.index === index) state.splice(index - 1, 1);
-        return book;
-      });
+      return state.filter((book) => book.id !== action.payload.id);
     default:
       return state;
   }
