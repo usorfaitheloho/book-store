d83b0bf64f541563fdeb2bfb0b0d89b5ed858a52
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-07 11:52:06 -0700

    Add getBooks

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,17 +3,32 @@ import API_URL from "../../Database/database";
 
 const ADD_BOOK = 'book-store/books/ADD_BOOK';
 const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
+const GET_BOOK = 'book-store/books/GET_BOOK';
 
 const initialState = [];
 
 export const addbook = (book) => async(dispatch) => {
-  await axios(API_URL)
+  await axios.post(API_URL)
     .then((response) => response.json())
     .then(
       () => dispatch({ type: ADD_BOOK, payload:book }),
       () => dispatch({ type: ADD_BOOK, payload:null }), 
     );
 };
+
+export const getBooks = () => async (dispatch) => {
+  await axios.get(API_URL)
+    .then((books) => books.json())
+    .then(
+      (data) => dispatch({ type: GET_BOOK, payload: data }),
+      () => dispatch({ type: GET_BOOK, payload:[] }), 
+    );
+};
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
